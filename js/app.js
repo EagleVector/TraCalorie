f35a5f74ffc219344ee83e5327fbfae62b0a84a1
EagleVector <kumarsubhambili3@gmail.com>
2023-06-17 18:19:00 +0530

    Adding Event Listeners to the application

--- js/app.js
+++ js/app.js
@@ -105,20 +105,83 @@ class Workout {
   }
 }
 
-const tracker = new CalorieTracker();
+// const tracker = new CalorieTracker();
 
-const breakFast = new Meal('BreakFast', 400);
-tracker.addMeal(breakFast);
+// const breakFast = new Meal('BreakFast', 400);
+// tracker.addMeal(breakFast);
 
-const lunch = new Meal('Lunch', 500);
-tracker.addMeal(lunch);
+// const lunch = new Meal('Lunch', 500);
+// tracker.addMeal(lunch);
 
-const run = new Workout('Morning Run', 300);
-tracker.addWorkout(run);
+// const run = new Workout('Morning Run', 300);
+// tracker.addWorkout(run);
 
-const pushUps = new Workout('Push-Ups', 200);
-tracker.addWorkout(pushUps);
+// const pushUps = new Workout('Push-Ups', 200);
+// tracker.addWorkout(pushUps);
 
-console.log(tracker._meals);
-console.log(tracker._workouts);
-console.log(tracker._totalCalories);
+// console.log(tracker._meals);
+// console.log(tracker._workouts);
+// console.log(tracker._totalCalories);
+
+// Adding event listenersto the Application
+
+class App {
+  constructor() {
+    this._tracker = new CalorieTracker();
+
+    document.getElementById('meal-form').addEventListener('submit', this._newMeal.bind(this));
+    document.getElementById('workout-form').addEventListener('submit', this._newWorkout.bind(this));
+  }
+
+  _newMeal(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('meal-name');
+    const calories = document.getElementById('meal-calories');
+
+    // Validate inputs
+    if (name.value === '' || calories.value === '') {
+      alert('Please enter the input fields');
+      return;
+    } 
+
+    const meal = new Meal(name.value, +calories.value);
+
+    this._tracker.addMeal(meal);
+
+    name.value = '';
+    calories.value = '';
+
+    const collapseMeal = document.getElementById('collapse-meal');
+    const bsCollapse = new bootstrap.Collapse(collapseMeal, {
+      toggle: true
+    });
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('workout-name');
+    const calories = document.getElementById('workout-calories');
+
+    // Validate inputs
+    if (name.value === '' || calories.value === '') {
+      alert('Please enter the input fields');
+      return;
+    } 
+
+    const workout = new Workout(name.value, +calories.value);
+
+    this._tracker.addWorkout(workout);
+
+    name.value = '';
+    calories.value = '';
+
+    const collapseWorkout = document.getElementById('collapse-workout');
+    const bsCollapse = new bootstrap.Collapse(collapseWorkout, {
+      toggle: true
+    });
+  }
+}
+
+const app = new App();
