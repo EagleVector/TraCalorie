0e3889fd9254164348112b40349094d32045ff32
EagleVector <kumarsubhambili3@gmail.com>
2023-06-18 23:20:54 +0530

    RESET

--- js/app.js
+++ js/app.js
@@ -51,6 +51,13 @@ class CalorieTracker {
     }
   }
 
+  reset() {
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+    this._render();
+  }
+
   // Private Methods 
 
   _displayCaloriesTotal() {
@@ -209,6 +216,7 @@ class App {
     document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
     document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'));
     document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
+    document.getElementById('reset').addEventListener('click', this._reset.bind(this));
   }
 
   _newItem(type, e) {
@@ -262,7 +270,15 @@ class App {
         item.style.display = 'none';
       }
     });
-  } 
+  }
+
+  _reset() {
+    this._tracker.reset();
+    document.getElementById('meal-items').innerHTML = '';
+    document.getElementById('workout-items').innerHTML = '';
+    document.getElementById('filter-meals').innerHTML = '';
+    document.getElementById('filter-workouts').innerHTML = '';
+  }
 }
 
 const app = new App();
